4c9e2210e562d6efbc5e67c77b637015e4bc00f2
aman <aman.nagle1999@gmail.com>
2022-05-23 19:38:27 +0530

    added operator function

--- javascript.js
+++ javascript.js
@@ -18,3 +18,25 @@ function modulo(num1,num2)
 {
     console.log(num1%num2);
 }
+function operator(num1,num2,operator)
+{
+    switch(operator)
+    {
+        case '+' :
+            add(num1,num2);
+            break;
+        case '-' :
+            subtract(num1,num2);
+            break;
+        case '*' :
+            multiply(num1,num2);
+            break;
+        case '/' :
+            divide(num1,num2);
+            break;
+        case '%' :
+            modulo(num1,num2);
+            break;        
+    }
+
+}
